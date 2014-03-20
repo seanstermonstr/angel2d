30a452eb9cd6fffed25b4576db5552c46073586f
Seanstermonstr <sean@sean-virtual-machine.(none)>
2014-03-20 12:11:47 -0700

    Updated Units.h and Units.cpp and makefile

--- Code/ClientGame/Units.cpp
+++ Code/ClientGame/Units.cpp
@@ -1,55 +1,111 @@
-#include <Units.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include "Units.h"
 using namespace std;
-
-
+#include <string>
 
 Units::Units()
 {
 	status = "Fine";
 	nation = "RAF";
-	visible = true;	
+	visibleStatus = true;	
 }
 
-Units::Units(string newStatus, string newNation, bool newVisible)
+Units::Units(string newStatus, string newNation, bool newVisibleStatus)
 {
 	status = newStatus;
 	nation = newNation;
-	visible = newVisible;
+	visible = newVisibleStatus;
 }
+
+string Units::getStatus()
+{
+	return status;
+}
+
+string Units::getNation()
+{
+	return nation;
+}
+
+bool Units::visible()
+{
+	return visibleStatus;
+}
+
 
 int Units::diceRoll()
 {
 	int i;
 	unsigned seed; 
 	
-	return (1 + (rand() % 6))
+	return (1 + (rand() % 6));
 }
 
-void Units::combat(int combatValue, string combatType)
+damage Units::airCombat(int combatValue)
 {
-	int damageCount = 0;
-	int abortCount = 0;
-	int destroyedCount = 0;
+	damage damageInfo;
+	damageInfo.aborted = 0;
+	damageInfo.damaged = 0;
 
-	if (combatType == 'AirToAir')
+	for (int i=0; i<combatValue; i++)
 	{
-		for (int i = 0; i <CombatValue; i++)
-		{
-			if (diceRoll() >= 4)
-				damageCount++;
-		}
-		return damageCount;	
+		if (diceRoll() == 4)
+			damageInfo.aborted++;
+		else if (diceRoll() >= 5)
+			damageInfo.damaged++;	
 	}
-	else if (combatType == 'Bombing')
+
+	return damageInfo;
+}
+
+damage Units::bombingCombat(int combatValue)
+{
+	damage damageInfo;
+	damageInfo.aborted = 0;
+	damageInfo.damage = 0;
+
+	for (int i=0; i<combatValue; i+++)
 	{
-		for (int i = 0; i <CombatValue; i++)
-		{
-			if (diceRoll() >= 3)
-				damageCount = 1;	
-		}
-		return damageCount;
+		if (diceRoll() == 3)
+			damageInfo.aborted++;
+		else if (diceRoll() >= 4)
+			damageInfo.damaged++;
+	}
+	
+	return damageInfo;
+}
+
+damage Units::aaCombat(int combatValue)
+{
+	damage damageInfo;
+	damageInfo.aborted = 0;
+	damageInfo.damage = 0;
+
+	for (int i=0; i<combatValue; i+++)
+	{
+		if (diceRoll() == 5)
+			damageInfo.aborted++;
+		else if (diceRoll() == 6)
+			damageInfo.damaged++;
+	}
+	
+	return damageInfo;
+}
+
+bool Units::bombingTarget(int combatValue)
+{
+	damage damageInfo;
+	damageInfo.aborted = 0;
+	damageInfo.damage = 0;
+
+	for (int i=0; i<combatValue; i+++)
+	{
+		if (diceRoll() >= 4)
+			return true;
 	}
 }
+
+
+
 
--- Code/ClientGame/Units.h
+++ Code/ClientGame/Units.h
@@ -1,22 +1,42 @@
-#include <String.h>
+#include <string>
+
+
+struct damage{
+	int aborted;
+	int damaged;
+};
 
 class Units
 {
-public: 
-	Units();  //First constructor
-	Units(string newStatus, string newNation, bool visible);  //Second constructor
-	
-	string getStatus();  //Retrieves the status of the unit
-	string getNation();  //Retrieves the nation of the unit
-	bool visible();      //Retrieves the visibility status of the unit	
-
-
-	void combat(int aCombatValue, int dCombatValue, string combatType);			//Handles combat between all types of units and all types of combat
-
-	
-
 private:
-	string status;		//i.e Destroyed, Damaged, Aborted, Fine
-	string nation;		//RAF or Luftwaffe
-	bool visible;		
+	std::string status;		//i.e Destroyed, Damaged, Aborted, Fine
+	std::string nation;		//RAF or Luftwaffe
+	bool visibleStatus;		//Indicates if a unit is visible or not
+
+public: 
+	Units();  
+	//First constructor method to intialize a Units object
+
+	Units(std::string newStatus, std::string newNation, bool newVisibleStatus);
+	//Second constructor method to intialize a Units object
+	
+	std::string getStatus();  //Retrieves the status of the unit
+	std::string getNation();  //Retrieves the nation of the unit
+	bool visible();      //Retrieves the visibility status of the unit	
+	
+	int diceRoll();
+	//Imitates and returns the value of a 6d dice roll
+
+	damage airCombat(int combatValue);
+	//Calculates how much damage is dealt in an airCombat round
+
+	damage bombingCombat(int combatValue);
+	//Calculates how much damage is dealt in a bombingCombat round
+
+	bool bombingTarget(int combatValue);
+	//Calculates if a bombing operation is successful
+	
+	damage aaCombat(int combatValue);
+	//Calculates how much damage is dealt in an aaCombat round
+
 };
