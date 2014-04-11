4f87ec5fb026424c38bdaea7a177f74607b8dc70
Seanstermonstr <sean@sean-virtual-machine.(none)>
2014-04-10 22:04:41 -0700

    update

--- Code/ClientGame/Main.cpp
+++ Code/ClientGame/Main.cpp
@@ -13,7 +13,7 @@ int main(int argc, char* argv[])
 	//		bool antiAliasing		default: false
 	//		bool fullScreen			default: false
 	//		bool resizable			default: false
-	theWorld.Initialize(1024,768,"Eagle Day",false);
+	theWorld.Initialize(1440,900,"Eagle Day",false);
 	
 	//adds the default grid so you can more easily place Actors
 	theWorld.Add(new GridActor(), -1);
